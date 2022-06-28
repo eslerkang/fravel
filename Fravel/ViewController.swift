58233f6312bff3332cd05d28c869baf9ba81d3d1
eslerkang <eslerkang@gmail.com>
2022-06-29 07:31:24 +0900

    initial setting

---
+++ Fravel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fravel
+//
+//  Created by 강태준 on 2022/06/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

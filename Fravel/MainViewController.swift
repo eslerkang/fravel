9bd62a4b98c0ac61f9e45d6cb0bd6378ae689f53
eslerkang <eslerkang@gmail.com>
2022-07-01 09:02:21 +0900

    feat: add scrollView at MainUIViewController 	- create function to add NoticeTableView -> not completed

--- Fravel/MainViewController.swift
+++ Fravel/MainViewController.swift
@@ -8,21 +8,67 @@
 import UIKit
 
 class MainViewController: UIViewController {
-    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var stackView: UIStackView!
+    
+    let tableView: UITableView = {
+        let tv = UITableView()
+        tv.separatorStyle = .none
+        tv.allowsSelection = false
+        return tv
+    } ()
+    
+    let noticeCellId = "NoticeUITableVIewCell"
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        configureSearchBar()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    
-    func configureSearchBar() {
+        
+        removeAllSubViews()
+        setupTableView()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
+    }
+    
+    func removeAllSubViews() {
+        for subView in stackView.arrangedSubviews {
+            subView.removeFromSuperview()
+        }
+    }
+    
+    func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(NoticeTableViewCell.self, forCellReuseIdentifier: noticeCellId)
+        stackView.addArrangedSubview(tableView)
+        print(stackView.arrangedSubviews)
+    }
+}
+
+extension MainViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: noticeCellId) as? NoticeTableViewCell else {return UITableViewCell()}
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "hihi"
     }
 }
---
+++ Fravel/NoticeTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  NoticeTableViewCell.swift
+//  Fravel
+//
+//  Created by 강태준 on 2022/07/01.
+//
+
+import UIKit
+
+class NoticeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}

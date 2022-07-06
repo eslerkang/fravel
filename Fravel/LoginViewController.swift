cf1e168dda8c503affd3d91a3de2d8401c9ad9b6
eslerkang <eslerkang@gmail.com>
2022-07-06 09:00:18 +0900

    feat: add Apple ID login, Google login 	- add breif profileView for login test

--- Fravel/LoginViewController.swift
+++ Fravel/LoginViewController.swift
@@ -6,10 +6,19 @@
 //
 
 import UIKit
+import CryptoKit
+import AuthenticationServices
+
+import FirebaseCore
+import GoogleSignIn
+import FirebaseAuth
+
 
 class LoginViewController: UIViewController {
-    @IBOutlet weak var googleLoginButton: UIButton!
+    @IBOutlet weak var googleLoginButton: GIDSignInButton!
     @IBOutlet weak var appleLoginButton: UIButton!
+    
+    fileprivate var currentNonce: String?
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -19,6 +28,14 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if let _ = Auth.auth().currentUser {
+            self.moveToTabBarController()
+        }
     }
     
     func configureButtons() {
@@ -31,12 +48,157 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func tapGoogleLoginButton(_ sender: UIButton) {
+        guard let clientID = FirebaseApp.app()?.options.clientID else {return}
+        let config = GIDConfiguration(clientID: clientID)
+        
+        GIDSignIn.sharedInstance.signIn(with: config, presenting: self) { [weak self] user, error in
+            guard let self = self else {return}
+            if let error = error {
+                self.showMessagePrompt("ERROR: \(error.localizedDescription)")
+                return
+            }
+            
+            guard
+                let authentication = user?.authentication,
+                let idToken = authentication.idToken
+            else {
+                return
+            }
+            
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: authentication.accessToken)
+            
+            Auth.auth().signIn(with: credential) { [weak self] authResult, error in
+                guard let self = self else {return}
+                if let error = error {
+                    self.showMessagePrompt("ERROR: \(error.localizedDescription)")
+                    return
+                }
+                self.moveToTabBarController()
+            }
+        }
+    }
+    
+    @IBAction func tapAppleLoginButton(_ sender: UIButton) {
+        startSignInWithAppleFlow()
+    }
+    
+    func showMessagePrompt(_ message: String) {
+      let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+      let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+      alert.addAction(okAction)
+      present(alert, animated: false, completion: nil)
+    }
+    
+    func moveToTabBarController() {
         guard let tabBarController = storyboard?.instantiateViewController(withIdentifier: "TabBarController") as? TabBarViewController else {return}
         tabBarController.modalPresentationStyle = .fullScreen
         self.present(tabBarController, animated: true)
     }
+}
+
+
+// Apple Login
+extension LoginViewController {
+    @available(iOS 13, *)
+    private func sha256(_ input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        let hashString = hashedData.compactMap {
+          String(format: "%02x", $0)
+      }.joined()
+        return hashString
+    }
     
-    @IBAction func tapAppleLoginButton(_ sender: UIButton) {
+    @available(iOS 13, *)
+    func startSignInWithAppleFlow() {
+        let nonce = randomNonceString()
+        currentNonce = nonce
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        request.nonce = sha256(nonce)
+
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
+    
+    private func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        let charset: [Character] =
+        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        var result = ""
+        var remainingLength = length
+
+        while remainingLength > 0 {
+            let randoms: [UInt8] = (0 ..< 16).map { _ in
+                var random: UInt8 = 0
+                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+                if errorCode != errSecSuccess {
+                    fatalError(
+                        "Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)"
+                    )
+                }
+                return random
+            }
+
+            randoms.forEach { random in
+                if remainingLength == 0 {
+                    return
+                }
+                if random < charset.count {
+                    result.append(charset[Int(random)])
+                    remainingLength -= 1
+                }
+            }
+        }
+        
+        return result
     }
 }
 
+
+extension LoginViewController: ASAuthorizationControllerDelegate {
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            guard let nonce = currentNonce else {
+                fatalError("Invalid state: A login callback was received, but no login request was sent.")
+            }
+            
+            guard let appleIDToken = appleIDCredential.identityToken else {
+                print("Unable to fetch identity token")
+                return
+            }
+            
+            guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+                print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+                return
+            }
+            
+            let credential = OAuthProvider.credential(withProviderID: "apple.com",
+                                                      idToken: idTokenString,
+                                                      rawNonce: nonce)
+            
+            Auth.auth().signIn(with: credential) { [weak self] (authResult, error) in
+                guard let self = self else {return}
+                if let error = error {
+                    self.showMessagePrompt("ERROR: \(error.localizedDescription)")
+                    return
+                }
+                self.moveToTabBarController()
+            }
+        }
+    }
+
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        print("Sign in with Apple errored: \(error)")
+    }
+}
+
+
+extension LoginViewController: ASAuthorizationControllerPresentationContextProviding {
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
+}

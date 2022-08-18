2d3ac3a1ce236257e55cf93307db0e2f9c4c9a6b
eslerkang <eslerkang@gmail.com>
2022-08-19 00:38:40 +0900

    feat: connect to User Information

--- Fravel/View/ProfileViewController.swift
+++ Fravel/View/ProfileViewController.swift
@@ -16,16 +16,20 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var editUserInfoButton: UIBarButtonItem!
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var profileImageContainer: UIView!
-    
-    var user: User?
+    @IBOutlet weak var displayNameField: UITextField!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     let storage = Storage.storage()
     let db = Firestore.firestore()
+    
+    var idEditing = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         getUser()
+        configureScrollView()
+        configureTextField()
     }
     
     private func getUser() {
@@ -44,13 +48,14 @@ class ProfileViewController: UIViewController {
                 return
             }
             
-            let userId = document.documentID
             guard let displayname = data["displayname"] as? String,
                   let image = data["image"] as? String
             else {
                 print("ERROR: Invalid User Info")
                 return
             }
+            
+            self.displayNameField.text = displayname
             
             let imageRef = self.storage.reference(forURL: image)
             imageRef.downloadURL { url, error in
@@ -61,24 +66,40 @@ class ProfileViewController: UIViewController {
                 guard let url = url else {
                     return
                 }
-                
-                self.user = User(id: userId, displayname: displayname, image: url)
-                
-                self.configureView()
+                                
+                DispatchQueue.main.async {
+                    self.profileImageView.kf.setImage(with: url)
+                    
+                    self.profileImageView.layer.cornerRadius = self.profileImageView.frame.width / 2.0
+                    self.profileImageView.layer.masksToBounds = true
+                    self.profileImageView.contentMode = .scaleAspectFill
+                }
             }
         }
     }
     
-    private func configureView() {
-        guard let user = user else {
-            return
-        }
-        
-        profileImageView.kf.setImage(with: user.image)
-        profileImageView.layer.cornerRadius = profileImageView.frame.width / 2.0
-        profileImageView.layer.masksToBounds = true
-        profileImageView.contentMode = .scaleAspectFill
-        
-        profileImageContainer.isHidden = false
+    private func configureTextField() {
+        displayNameField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged
+        )
+    }
+    
+    private func configureScrollView() {
+        self.scrollView.delegate = self
+    }
+    
+    @objc private func textFieldDidChange(_ textField: UITextField) {
+        editUserInfoButton.isEnabled = !(displayNameField.text?.isEmpty ?? true)
+    }
+    
+    @IBAction func tapEditButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tapProfileImageEditButton(_ sender: UIButton) {
+    }
+}
+
+extension ProfileViewController: UIScrollViewDelegate {
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        self.view.endEditing(true)
     }
 }
